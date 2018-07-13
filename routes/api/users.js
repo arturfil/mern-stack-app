5114cdd132b088084b816f25a4c8f02267e5bdaf
Arturo Filio Villa <arturfil@hotmail.com>
2018-07-13 14:45:19 -0400

    initial setup of the server and routing

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../../models/User');
+
 // @route GET api/users/test
 router.get('/test', (req, res, next) => res.json({msg: "Users Works"}));
+
+// @route GET api/users/register
+router.post('/register', (req, res, next) => {
+    User.findOne({ email: req.body.email });
+});
 
 module.exports = router;
