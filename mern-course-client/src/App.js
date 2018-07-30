1f8e003ee5b2110fd7fba13bbd886d34df3ee3f6
Arturo Filio Villa <arturfil@hotmail.com>
2018-07-30 04:55:41 -0400

    first setup for redux

--- mern-course-client/src/App.js
+++ mern-course-client/src/App.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+// import store file
+import store from './store';
 
 //import Components
 import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer'
-import Landing from './components/layout/Landing'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 import './App.css';
 
 class App extends Component {
   render() {
-    return <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </BrowserRouter>;
+        </BrowserRouter>
+      </Provider>
+    );
   }
 }
 
