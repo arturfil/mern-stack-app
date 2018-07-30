ccba09c8931214e7fc98465970fadfec1774bb68
Arturo Filio Villa <arturfil@hotmail.com>
2018-07-30 02:12:16 -0400

    added state and onChange and onSubmit methods

--- mern-course-client/src/App.js
+++ mern-course-client/src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 //import Components
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+
+import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <Navbar/>
-        <Landing />
-        <Footer />
-      </div>
-    );
+    return <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </BrowserRouter>;
   }
 }
 
