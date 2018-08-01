33c53fe04557549a618ab5430f01cb45bed982e8
Arturo Filio Villa <arturfil@hotmail.com>
2018-08-01 19:05:12 -0400

    added state to navbar login and register components

--- mern-course-client/src/App.js
+++ mern-course-client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import setAuthToken from '../src/utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
 // import store file
 import store from './store';
 
@@ -13,6 +18,26 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
 import './App.css';
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
