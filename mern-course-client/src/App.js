597e23f4f0d23a63c3e9b9ea7ef423f4e4ffb0aa
Arturo Filio Villa <arturfil@hotmail.com>
2018-08-02 01:29:42 -0400

    added a profileAction and profileReducer component in order to give state to the dashboard and feed the state whether the user is logged in or not, also created PrivateRoutes component

--- mern-course-client/src/App.js
+++ mern-course-client/src/App.js
@@ -6,6 +6,7 @@ import setAuthToken from '../src/utils/setAuthToken';
 import jwt_decode from 'jwt-decode';
 
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 // import store file
 import store from './store';
@@ -16,6 +17,7 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
@@ -34,6 +36,7 @@ if (localStorage.jwtToken) {
     // Logout user
     store.dispatch(logoutUser());
     // TODO: Clear current Profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = '/login';
   }
@@ -50,6 +53,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
---
+++ mern-course-client/src/components/common/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const PrivateRoute = () => {
+  return <div />;
+};
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => {
+  auth: state.auth;
+};
+
+export default connect(mapStateToProps)(PrivateRoute);
